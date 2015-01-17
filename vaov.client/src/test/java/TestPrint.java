0f20046f40eb3d28f508ab3145f4b7b20da64826
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2015-01-17 20:08:03 +0100

    moved Account.print to new class PrintService

--- vaov.client/src/test/java/TestPrint.java
+++ vaov.client/src/test/java/TestPrint.java
@@ -1,7 +1,3 @@
-import static org.junit.Assert.*;
-import gnu.jpdf.PDFTest;
-
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -9,14 +5,16 @@ import org.junit.Test;
 
 import vaov.client.account.Account;
 import vaov.client.util.KeyException;
+import vaov.client.util.PrintService;
 
 public class TestPrint {
 
 	@Test
 	public void testPrintAccount() throws KeyException, IOException {
-		Account a = new Account("i9YMFc+vWTpO3B5hxwsg80UvHX6sGUKdhyly7RQOfg230UZfaVfN6fqHMtfb3bxydpRUEuQvgKgW450FwzrSww==");
+		Account a = new Account(
+				"i9YMFc+vWTpO3B5hxwsg80UvHX6sGUKdhyly7RQOfg230UZfaVfN6fqHMtfb3bxydpRUEuQvgKgW450FwzrSww==");
 		FileOutputStream out = new FileOutputStream("test_files/account.pdf");
-		a.print(out);
+		PrintService.print(out, a.getHash());
 		out.close();
 	}
 
