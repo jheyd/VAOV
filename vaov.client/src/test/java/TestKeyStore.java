a5bd7d2c0a2bb29c3542bd80aad563a373384c8c
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2015-01-17 11:56:19 +0100

    fixed test

--- vaov.client/src/test/java/TestKeyStore.java
+++ vaov.client/src/test/java/TestKeyStore.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
@@ -17,13 +17,17 @@ public class TestKeyStore {
 
 	@Test
 	public void loadAccount() throws KeyException {
-		PrivateAccount pa = new PrivateAccount("testAccount", "foobar".toCharArray());
+		Helper.initKeyStore("foobar".toCharArray());
+		PrivateAccount pa = new PrivateAccount();
+		pa.store("testAccount", "foobar".toCharArray());
+		new PrivateAccount("testAccount", "foobar".toCharArray());
 	}
-	
+
 	@Test
 	public void loadWrongAccount() {
 		try {
-			PrivateAccount pa = new PrivateAccount("testWrongAccount", "foobar".toCharArray());
+			PrivateAccount pa = new PrivateAccount("testWrongAccount",
+					"foobar".toCharArray());
 			assertFalse("Must throw exception!", true);
 		} catch (KeyException e) {
 			// must be thrown
